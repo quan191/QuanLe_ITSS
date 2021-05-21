3de55b217db2ad18300fb38cf686577104c2476c
EC2 Default User <ec2-user@ip-172-31-58-212.ec2.internal>
2021-05-21 10:37:59 +0000

    save avatar by storage

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB6Fz4m6Yt9hH2rdzdzv-MjgurI8DiBnjo",
-    authDomain: "fir-sample-5c866.firebaseapp.com",
-    projectId: "fir-sample-5c866",
-    storageBucket: "fir-sample-5c866.appspot.com",
-    messagingSenderId: "361833893664",
-    appId: "1:361833893664:web:50eee0942e1685e2fbd145"
+    apiKey: "AIzaSyDGc1yOau8koRrocY4VhGxN2v6rKzeXC1U",
+    authDomain: "fir-sample-da2d7.firebaseapp.com",
+    projectId: "fir-sample-da2d7",
+    storageBucket: "fir-sample-da2d7.appspot.com",
+    messagingSenderId: "704554063752",
+    appId: "1:704554063752:web:31d269bb31bc47275ecc3d"
   };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
